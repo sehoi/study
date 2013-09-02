02444c8317c48268641896836569f247072b6f43
kaiser38 <kaiser308@gmail.com>
2013-09-02 19:18:07 +0900

    test

---
+++ nlp/parse/parser.py
@@ -0,0 +1,10 @@
+﻿# -*- coding: utf-8 -*-
+import os
+from morph import morpher
+
+sentences = []
+punction_delims = ['.', '?', '!']
+
+def get_senetences(contents):
+	contents.find
+	return sentences
--- nlp/test.py
+++ nlp/test.py
@@ -2,29 +2,77 @@
 import os
 from morph import morpher
 
-def test_morpher(filename):
+lines = []
+line_words_list = []
+word_list = []
+
+word_tf = {}
+keyword_tf = {}
+
+line_score = []
+
+tf_limit = 3
+score_limit = 23
+
+max_lines = 3
+
+def test(filename):
 	input_file = open(filename, 'r')
 	lines = input_file.readlines()
+	input_file.close()
+	
+	# parsing
 	for line in lines:
-		line = morpher.remove_simbols(line)
 		if len(line) == 0:
-			continue	
+			continue
 		print("line: ", line)
 		
-		word_list = morpher.get_words_in_line(line)
-		if len(word_list) != 0:
-			print("words: ", word_list)
+		line = morpher.remove_simbols(line)
+		line = morpher.get_words_in_line(line)
+		line_words = []
+		for word in line:
+			word = morpher.remove_josaeomi(word)
+			line_words.append(word)
+			word_list.append(word)
 		
-		removed_word_list = []
-		for word in word_list:
-			removed_word = morpher.remove_josaeomi(word)
-			if len(removed_word) == 0:
-				continue
-			removed_word_list.append(removed_word)
-		print("refined_words: ", removed_word_list)
+		line_words_list.append(line_words)
+	
+	# tf count
+	for word in word_list:
+		if len(word) == 0:
+			continue
 		
-		print('\n')
-	input_file.close();
-
+		tf = word_list.count(word)
+		word_tf[word] = tf
+	print(word_tf)
+	
+	# get keywords
+	keys = sorted(word_tf.keys())
+	for key in keys:
+		tf = word_tf[key]
+		
+		if tf >= tf_limit:
+			keyword_tf[key] = tf
+			print(key, tf)
+	
+	# line scoring
+	keywords = keyword_tf.keys()
+	for line_words in line_words_list:
+		score = 0
+		for keyword in keywords:
+			tf = keyword_tf[keyword]
+			if keyword in line_words:
+				score += tf
+			print(keyword, tf)
+		line_score.append(score)
+	
+	print(line_score)
+	
+	# get high score line
+	for line in lines:
+		index = lines.index(line)
+		if line_score[index] >= score_limit:
+			print(line)
+	
 if __name__ == '__main__':
-	test_morpher(os.path.dirname(__file__)+os.sep+'input'+os.sep+'input2.out')
+	test(os.path.dirname(__file__)+os.sep+'input'+os.sep+'input2.out')
